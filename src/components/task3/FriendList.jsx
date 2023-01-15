4c07c39d3559687cbc7b53cb2db52e57525a9db0
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2023-01-15 16:41:18 +0200

    fix

--- src/components/task3/FriendList.jsx
+++ src/components/task3/FriendList.jsx
@@ -8,8 +8,13 @@ export const FriendList = ({ friends }) => {
   return <>
     <h2 id="task-3" className={css.title}>Friends List</h2>
     <ul className={css.list}>
-      {friends.map(({ id }) => 
-        <FriendListItem key={id}/>
+      {friends.map(({ id, isOnline, avatar, name }) => 
+        <FriendListItem
+          key={id} 
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
       )}
     </ul>
   </>
